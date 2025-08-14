f98436c9f296e90f5436a6640c754e3325c75138
userSTANRAGE <jokarikop@gmail.com>
2025-08-14 18:03:49 +0400

    UPD

---
+++ src/blocks/header/header.js
@@ -0,0 +1,12 @@
+(() => {
+
+    const header = document.querySelector('.header');
+    const headerBurger = header.querySelector('.header__burger');
+    const headerMenu = header.querySelector('.header__menu');
+
+    headerBurger.addEventListener('click', () => {
+        headerMenu.classList.toggle('active');
+        headerBurger.classList.toggle('active');
+    });
+
+})();
